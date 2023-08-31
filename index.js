99ce221d8027bb6edf912284361c0705bc34f4c2
AK-Polash <polashk199@gmail.com>
2023-09-01 04:15:46 +0600

    PORT added on dotenv

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(_);
-app.listen(8000);
+app.listen(process.env.PORT, () =>
+  console.log(`PORT IS RUNNING ON ${process.env.PORT}`)
+);
